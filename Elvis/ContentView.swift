0c227357a50e9d92aa0bc437bba904dec9caa8cf
Charles <zpiao@ascensionacademy.org>
2026-01-16 09:37:24 -0600

    First Commit!

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Text("What's so funny 'Bout")
+                .font(.largeTitle)
+                .foregroundStyle(.purple)
+            
+            
+            Image(systemName: "peacesign")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
