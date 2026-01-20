2d2ed707edb5f9164213f52fa736d7ce00807c45
Charles <zpiao@ascensionacademy.org>
2026-01-20 09:14:33 -0600

    Final Project!

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "peacesign"
+    @State private var caption = "Peace"
     var body: some View {
         VStack {
             Text("What's so funny 'Bout")
                 .font(.largeTitle)
+                .fontWeight(.heavy)
                 .foregroundStyle(.purple)
+            Spacer()
             
-            
-            Image(systemName: "peacesign")
+            Image(systemName: message)
+                .resizable()
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.black)
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            Text(caption)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.purple)
+            Spacer()
+            
+            HStack {
+                Button("Peace"){
+                    message = "peacesign"
+                    caption="Peace"
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+                Button("Love"){
+                    message="heart.fill"
+                    caption = "Love"
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+                Button("Understanding"){
+                    message = "lightbulb"
+                    caption = "Understanding"
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+            }
         }
         .padding()
     }
